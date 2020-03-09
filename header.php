2e097631ff12d9ee8c2415c4a034cdd8a1a91fa6
CyberValenok <alendorf.alex@yandex.ru>
2020-03-10 02:41:41 +0300

    Toha ento tebe

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+ <html lang="ru">
+  <head>
+   <meta charset="utf-8"> 
+   <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge">
+    <title>Bootstrap Template</title>
+     <link href="css/bootstrap.min.css" rel="stylesheet"> 
+</head>
+<body>
+    <div class="navbar navbar-inverse navbar-static-top">
+       <div class="container">
+            <div class="navber-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
+                    <span class="sr-only">Open navigation</span> 
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span> 
+                    <span class="icon-bar"></span> 
+                </button>
+                <a class="navbar-brand" href="/">My first blog</a>
+            </div>
+            <div class="collapse navbar-collapse" id="responsive-menu">
+            <ul class="nav navbar-nav"> 
+            <li><a href="#">Пункт 1</a> </li> 
+            <li><a href="#">Пункт 2</a> </li> 
+            <li><a href="#">Пункт 3</a> </li>
+            <li><a href="#">Пункт 4</a> </li>
+            </ul>
+            </div>
+         </div> 
+    </div> 
--- index.php
+++ index.php
@@ -1,47 +1,19 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tik-Toha-2</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css"/>
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,700,700italic|Playfair+Display:400,700&subset=latin,cyrillic">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
-</head>
-<body>
-    
-        <header>
-            <nav class="container">
-                <a class="logo" href="">
-                    <span>L</span>
-                    <span>O</span>
-                    <span>G</span>
-                    <span>O</span>
-                </a>
-                <div class="nav-toggle"><span></span></div>
-                <form action="" method="get" id="searchform">
-                    <input type="text" placeholder="Поиск тиктохи">
-                    <button type="submit"><i class="fa fa-search">search</i></button>
-                </form>
-                <ul>
-                    <li><a href="">Тик</a></li>
-                    <li><a href="">Тоха</a></li>
-                    <li><a href="">Тик-Тоха</a></li>
-                </ul>
-            </nav>
-        </header>
-        <div class="main">
-            <form action="" method="post" id="login">
-            <input type="text" placeholder="Username or email">
-            <input type="password" placeholder="Password">
-            <button type="submit">login</button>
-            </form>
-        </div>
-        <div class="content"></div>
-        <div class="footer"></div>
-   
-</body>
-</html>
+<?php
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+
+include 'header.php';
+
+$category_1= array('id'=>1,'category_title'=>'Путешествия', 'category_slag'=>'/travel');
+$category_2= array('id'=>2,'category_title'=>'Жизнь', 'category_slag'=>'/life');
+$category=array($category_1,$category_2)
+?>
+<div class="container">
+    <div class="alert alert-danger">
+    <pre><?= $category[0]['category_title'] ?></pre>
+    </div>
+</div>
+
+<?include 'footer.php';?>
+ 
